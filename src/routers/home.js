054a759b9b9b96be85b5c4db98fe72cd5e1b77ae
wordisstuff <wordisstuff@gmail.com>
2025-05-28 03:13:21 -0500

    wether sensors controller fix

--- src/routers/home.js
+++ src/routers/home.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { homeController } from '../controllers/home.js';
+import { homeController, sensorsController } from '../controllers/home.js';
 
 const router = Router();
 router.get('/', ctrlWrapper(homeController));
