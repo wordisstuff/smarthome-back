2ec3b9a0e48b877e65becd19e598297d9996c72b
wordisstuff <wordisstuff@gmail.com>
2025-06-01 15:31:16 -0500

    if function

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -8,5 +8,9 @@ export const sensorsController = async (req, res) => {
     console.log(req.body);
     if (token !== authDb.secret) return res.status(403).send('Access denied');
     console.log('Дані з ESP32:', soil_status, light, temp);
+    if (soil_status === 'dry') await valveTogle({ raley: 1, state: true });
+    if (soil_status === 'wet' || soil_status === 'in_water')
+        await valveTogle({ raley: 1, state: false });
+
     res.status(200).json({});
 };
