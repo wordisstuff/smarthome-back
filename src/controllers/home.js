7309d68a557dce4d3d46ac3359ef783abe1a2b23
wordisstuff <wordisstuff@gmail.com>
2025-06-01 15:17:29 -0500

    wether sensors controller fix add temp

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -4,8 +4,8 @@ export const homeController = async (req, res) => {
     res.status(200).json({});
 };
 export const sensorsController = async (req, res) => {
-    const { soil, light, token } = req.body;
+    const { soil_status, light, token, temp } = req.body;
     if (token !== authDb.secret) return res.status(403).send('Access denied');
-    console.log('Дані з ESP32:', soil, light);
+    console.log('Дані з ESP32:', soil_status, light, temp);
     res.status(200).json({});
 };
